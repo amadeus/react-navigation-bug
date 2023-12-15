378dc1926cf4d2ca4cb9f3e7504c72e6cbb9edcb
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2023-12-14 18:55:05 -0800

    Demo app setup

--- App.tsx
+++ App.tsx
@@ -1,118 +1,137 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Text, View, SafeAreaView, StyleSheet, Pressable} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer, useNavigation, type NavigationProp} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const styles = StyleSheet.create({
+  inner: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  innerModal: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pressable: {
+    padding: 20,
+    backgroundColor: '#ccc',
+    marginVertical: 5,
+  },
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  textIntro: {
+    padding: 10,
+  },
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  buttons: {
+    flexDirection: 'row',
+    padding: 10,
+  },
+
+  text: {
+    textAlign: 'center',
+  },
+});
+
+type RootStackScreens = {
+  Home: undefined;
+  Modal: undefined;
+  TransparentModal: undefined;
+  ContainedTransparentModal: undefined;
+};
+
+const RootStack = createNativeStackNavigator<RootStackScreens>();
+
+function Home() {
+  const navigation = useNavigation<NavigationProp<RootStackScreens>>();
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.inner}>
+        <Text style={styles.textIntro}>
+          Red represents the safe area padding as provided by React Native Safe Area Context (although I've noticed that
+          the issue also affects the build in react native SafeArea component).
+        </Text>
+        <Text style={styles.textIntro}>
+          This only applies to iOS. Ensure you have rotation lock off, and rotate the view into landscape orientation.
+          Note how the red safe areas appear. Then tap `Spawn Transparent Modal`, dismiss the modal, and then rotate the
+          screen again to see how the safe areas are now stuck as the portrait values. You must force quite the app to
+          undo the bug.
+        </Text>
+        <Pressable style={styles.pressable} onPress={() => navigation.navigate('Modal')}>
+          <Text style={styles.text}>"modal"</Text>
+        </Pressable>
+        <Pressable style={styles.pressable} onPress={() => navigation.navigate('ContainedTransparentModal')}>
+          <Text style={styles.text}>"containedTransparentModal"</Text>
+        </Pressable>
+        <Pressable style={styles.pressable} onPress={() => navigation.navigate('TransparentModal')}>
+          <Text style={styles.text}>"transparentModal"</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+function Modal() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.innerModal}>
+      <Text>Modal</Text>
+      <Pressable style={styles.pressable} onPress={() => navigation.goBack()}>
+        <Text>Go Back!</Text>
+      </Pressable>
+    </View>
+  );
+}
 
-export default App;
+function TransparentModal() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.innerModal}>
+      <Text>Transparent Modal</Text>
+      <Pressable style={styles.pressable} onPress={() => navigation.goBack()}>
+        <Text>Go Back!</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+function ContainedTransparentModal() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.innerModal}>
+      <Text>Contained Transparent Modal</Text>
+      <Pressable style={styles.pressable} onPress={() => navigation.goBack()}>
+        <Text>Go Back!</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootStack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+          <RootStack.Screen name="Home" component={Home} />
+          <RootStack.Screen
+            name="TransparentModal"
+            component={TransparentModal}
+            options={{presentation: 'transparentModal'}}
+          />
+          <RootStack.Screen
+            name="ContainedTransparentModal"
+            component={ContainedTransparentModal}
+            options={{presentation: 'containedTransparentModal'}}
+          />
+          <RootStack.Screen name="Modal" component={Modal} options={{presentation: 'modal'}} />
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
